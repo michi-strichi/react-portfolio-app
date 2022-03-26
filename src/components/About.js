8a71fa3617fa6d52ad3c1c1245d828aae5b2452e
Michael-J-Hochreiter <michael.j.hochreiter@gmail.com>
2022-03-26 12:02:25 +0100

    added first three.js thing

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber';
+// import { OrbitControls } from 'drei';
+// import { boxBufferGeometry } from 'three';
+
 import './styles/About.css';
 import Footer from './Footer';
 
+const Box = () => {
+  return (
+    <mesh>
+      <sphereGeometry args={[1, 32, 32]} />
+      <meshPhongMaterial color="red" />
+    </mesh>
+  )
+}
+
 const About = () => {
   return (
-    <div className='About'>Abouttttt me
+    <div className='About'>
+      <div className='AboutBio'>
+        Hi, I'm Michael Hochreiter and im really interested in ....
+      </div>
+      <div className='AboutPortrait'>
+        <Canvas>
+          <Box />
+        </Canvas>
+      </div>
       <Footer />
     </div>
   )
