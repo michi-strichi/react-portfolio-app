e5fd4191bae1d0285517304af0493dd3101bf627
Michael-J-Hochreiter <michael.j.hochreiter@gmail.com>
2022-03-28 12:51:26 +0200

    added svgs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,13 +11,24 @@ const Navbar = ({selected}) => {
         <nav className="NavbarNav">
             <ul className="NavbarList">
                 <li className="NavbarItem">
-                    <Link to='/work' onClick={() => setPage("work")} className={page === "work" ? "NavbarItemSelected" : "NavbarItemNotSelected"}>work</Link>
+                    <Link to='/work' onClick={() => setPage("work")} className={page === "work" ? "NavbarItemSelected" : "NavbarItemNotSelected"}>
+                        work
+                    </Link>
                 </li>
                 <li className="NavbarItem" >
-                    <Link to='/' onClick={() => setPage("home")} className={page === "home" ? "NavbarItemSelected" : "NavbarItemNotSelected"}>home</Link>
+                    <Link to='/' onClick={() => setPage("home")} className={page === "home" ? "NavbarItemSelected" : "NavbarItemNotSelected"}>
+                        <img src="./assets/Kleeblatt_bold.svg"  />
+                        {/* <img src={page === "home" ? "./assets/Kleeblatt_bold.svg" : "./assets/Kleeblatt_medium.svg"} /> */}
+                        {/* <object data={page === "home" ? "./assets/Kleeblatt_bold" : "./assets/Kleeblatt_medium"} width="300" height="300"> </object> */}
+                    </Link>
                 </li>   
                 <li className="NavbarItem">
-                    <Link to='/about' onClick={() => setPage("about")} className={page === "about" ? "NavbarItemSelected" : "NavbarItemNotSelected"}>about</Link>
+                    <Link to='/about' onClick={() => setPage("about")} className={page === "about" ? "NavbarItemSelected" : "NavbarItemNotSelected"}>
+                        about
+                    </Link>
+                </li>
+                <li>
+                    <img src="./assets/Kleeblatt_bold.svg" />
                 </li>
             </ul>
         </nav>
