d5fb7cf4088b2669516138b8b1b7f119fa0359fb
Michael-J-Hochreiter <michael.j.hochreiter@gmail.com>
2022-07-15 16:58:51 +0200

    worked on home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
 import React, { useState, useRef, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF, PerspectiveCamera } from '@react-three/drei';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 import './styles/Home.scss';
-import { randFloat } from 'three/src/math/MathUtils';
 
 
 function Dandelion_Model({ ...props }) {
@@ -21,7 +20,7 @@ function Klee_Model({ ...props }) {
     const { nodes, materials } = useGLTF('/planetModels_TEST/klee_TEST.glb')
     return (
         <group ref={group} {...props} dispose={null}>
-            <mesh position={props.pos} geometry={nodes.klee.geometry} material={materials.klee_mat} />
+            <mesh rotation={[0.8, -0.2, 0.5]} position={props.pos} geometry={nodes.klee.geometry} material={materials.klee_mat} />
         </group>
     )
 }
@@ -47,7 +46,7 @@ function Planet_Model({ ...props }) {
     const { nodes, materials } = useGLTF('planetModels_TEST//planet_TEST.glb')
     return (
         <group ref={group} {...props} dispose={null}>
-            <mesh position={props.pos} geometry={nodes.planet.geometry} material={materials.planet_mat} />
+            <mesh name={'planet'} position={props.pos} geometry={nodes.planet.geometry} material={materials.planet_mat} />
         </group>
     )
 }
@@ -57,16 +56,40 @@ useGLTF.preload('/planetModels_TEST/planet_TEST.glb')
 
 const Home = ({ theme }) => {
 
-    const [brush, setBrush] = useState('dandelion');
+    const [brush, setBrush] = useState('klee');
 
     const [klees, setKlees] = useState([]);
     // const [dandelions, setDandelions] = useState([]);
     // const [mushrooms, setMushrooms] = useState([]);
 
-    const addKlee = (pos) => {
-        setKlees([...klees, pos]);
+    const addKlee = (pos, rot) => {
+        setKlees([...klees, [pos, rot]]);
         console.log(klees);
-    }
+    };
+
+
+    const handlePointerDown = (e) => {
+        console.log(e);
+
+        const intersection = e.intersections[0];
+        
+        if (intersection.object.name === 'planet') {
+
+            switch (brush) {
+                case 'klee':
+                    console.log('adding klee');
+                    addKlee(intersection.point, intersection.face.normal);
+                    break;
+                case 'dandelion':
+                    console.log('adding dandelion');
+                    break;
+                case 'mushroom':
+                    console.log('adding mushroom');
+                    break;
+            }
+        }
+
+    };
 
     return (
         <div className={'Home' + ' ' + theme}>
@@ -77,13 +100,13 @@ const Home = ({ theme }) => {
 
             <div className='CanvasWrapper'>
                 <Canvas>
-                    <OrbitControls />   
+                    <OrbitControls />
                     <ambientLight intensity={1} />
                     <Suspense fallback={null}>
-                        <Planet_Model onClick={(e) => console.log("you clicked", e.object.name)}/>
+                        <Planet_Model onPointerUp={(e) => handlePointerDown(e)} />
 
                         {klees.map((klee, index) => (
-                            <Klee_Model key={index} pos={klee} />
+                            <Klee_Model key={index} pos={klee[0]} rot={klee[1]} />
                         ))}
 
                     </Suspense>
