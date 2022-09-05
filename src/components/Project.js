72465fee9ead6ce7e39495b34640c2c331f7df30
Michael-J-Hochreiter <michael.j.hochreiter@gmail.com>
2022-09-05 18:19:18 +0200

    worked on cursors

--- src/components/Project.js
+++ src/components/Project.js
@@ -15,6 +15,42 @@ const Project = ({ theme, title, year, description, link, color, software, role,
     const detail_0_loaded = require('./assets/images/' + detail_0);
     const detail_1_loaded = require('./assets/images/' + detail_1);
 
+    // ,
+    // {
+    //     "title":        "Soundreactive",
+    //     "year":         "2022",
+    //     "description":  "",
+    //     "link":         "",
+    //     "color":        "",
+    //     "software":     "Blender3D, vvvv",
+    //     "thumbnail":    "",
+    //     "detail_0":     "",
+    //     "detail_1":     ""
+    // },
+    // {
+    //     "title":        "ArtisVR",
+    //     "year":         "2022",
+    //     "description":  "",
+    //     "link":         "",
+    //     "color":        "",
+    //     "software":     "Blender3D, Unity",
+    //     "role":         "3D modelling, development",
+    //     "thumbnail":    "",
+    //     "detail_0":     "",
+    //     "detail_1":     ""
+    // },    
+    // {
+    //     "title":        "This Website",
+    //     "year":         "2022",
+    //     "description":  "I built this portfolio website mainly to show off my work but also to learn about frontend web dev and 3D in the web. All layouts, icons, images and 3D models are created by me with ❤️.",
+    //     "link":         "",
+    //     "color":        "",
+    //     "software":     "VS Code, React, three.js, Blender3D, Adobe Illustrator",
+    //     "thumbnail":    "",
+    //     "detail_0":     "",
+    //     "detail_1":     ""
+    // }
+
     const LinkStyle = {
             color: color,
       };
