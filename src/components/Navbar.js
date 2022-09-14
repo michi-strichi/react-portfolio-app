f447359adc1da6470eef77ccfeacc872a40e7a4c
Michael-J-Hochreiter <michael.j.hochreiter@gmail.com>
2022-09-15 00:24:22 +0200

    finally fixed blue kleeblatt?

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom'
 
 import './styles/Navbar.scss';
 import ThemeSwitch from './ThemeSwitch';
-import {ReactComponent as KleeblattThin} from './assets/svgs/Kleeblatt_thin.svg';
-import {ReactComponent as KleeblattBold} from './assets/svgs/Kleeblatt_bold.svg';
+import { ReactComponent as KleeblattThin } from './assets/svgs/Kleeblatt_thin.svg';
+import { ReactComponent as KleeblattBold } from './assets/svgs/Kleeblatt_bold.svg';
+import { NoToneMapping } from 'three';
 
 const Navbar = ({ page, theme, min781, setPage, setTheme }) => {
 
@@ -13,6 +14,15 @@ const Navbar = ({ page, theme, min781, setPage, setTheme }) => {
     const toggleNavbarStatus = () => {
         navbarStatus === 'Active' ? setNavbarStatus('Inactive') : setNavbarStatus('Active');
     }
+
+    const thinStyle = {
+        display: 'inline'
+    };
+
+    const boldStyle = {
+        display: 'inline'
+    };
+
 
     return (
         <div className={'Navbar' + ' ' + theme + ' ' + navbarStatus}>
@@ -27,17 +37,19 @@ const Navbar = ({ page, theme, min781, setPage, setTheme }) => {
                     <nav>
                         <ul>
                             <li className="kleeblatt">
-                                <Link to='/' onClick={() => { setPage("home"); setNavbarStatus('Inactive'); window.scrollTo({top: 0, left: 0, behavior: 'auto'}); }} className={page === "home" ? 'selected' : 'notSelected'}>
-                                    {page === "home" ? <KleeblattThin /> : <KleeblattBold />}
+                                <Link to='/' onClick={() => { setPage("home"); setNavbarStatus('Inactive'); window.scrollTo({ top: 0, left: 0, behavior: 'auto' }); }} className={page === "home" ? 'selected' : 'notSelected'}>
+                                    {/* {page === "home" ? <KleeblattBold /> : <KleeblattThin />} */}
+                                    <KleeblattBold style={page === 'home' ? {display: 'inline'} : {display: 'none'}} />
+                                    <KleeblattThin style={page === 'home' ? {display: 'none'} : {display: 'inline'}} />
                                 </Link>
                             </li>
                             <li>
-                                <Link to='/work' onClick={() => { setPage("work"); setNavbarStatus('Inactive'); window.scrollTo({top: 0, left: 0, behavior: 'auto'}); }} className={page === "work" ? 'selected' : 'notSelected'}>
+                                <Link to='/work' onClick={() => { setPage("work"); setNavbarStatus('Inactive'); window.scrollTo({ top: 0, left: 0, behavior: 'auto' }); }} className={page === "work" ? 'selected' : 'notSelected'}>
                                     work
                                 </Link>
                             </li>
                             <li>
-                                <Link to='/about' onClick={() => { setPage("about"); setNavbarStatus('Inactive'); window.scrollTo({top: 0, left: 0, behavior: 'auto'}); }} className={page === "about" ? 'selected' : 'notSelected'}>
+                                <Link to='/about' onClick={() => { setPage("about"); setNavbarStatus('Inactive'); window.scrollTo({ top: 0, left: 0, behavior: 'auto' }); }} className={page === "about" ? 'selected' : 'notSelected'}>
                                     about
                                 </Link>
 
